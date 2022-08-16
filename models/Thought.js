af8b81639b9b21cff2a3915f8919a7dd981af81e
Giovanni Claro <gio.claro316@gmail.com>
2022-08-16 09:28:08 -0400

    basic skeleton

---
+++ models/Thought.js
@@ -0,0 +1,45 @@
+const {Schema, model, Types} = require('mongoose');
+
+const ReactionSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectsId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get
+    }
+})
+
+const ThoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get
+    },
+    reactions: [ReactionSchema]
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
